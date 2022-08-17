4e5d67bd601e4a8519774e85ac4a2bdb277e314f
jooam <lord0325@naver.com>
2022-08-17 16:54:58 +0900

    feat: 페이지 기본 구성 설정

--- frontend/src/pages/RegionInfo.tsx
+++ frontend/src/pages/RegionInfo.tsx
@@ -1,5 +1,13 @@
 function RegionInfo() {
-  return <div>RegionInfo</div>;
+  return (
+    <div>
+      RegionInfo
+      <form>
+        <input type="text" />
+        <button type="submit">변경하기</button>
+      </form>
+    </div>
+  );
 }
 
 export default RegionInfo;
