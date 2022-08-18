249e39e17474490bfd2eb712e813c3846f06121e
jooam <lord0325@naver.com>
2022-08-18 17:08:07 +0900

    feat: mySQL 연결 및 기초 테이블 Scheme 생성

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MySQLModule } from './config/mysql.module';
+import { UserModule } from './domain/user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MySQLModule,
+    UserModule,
+  ],
 })
 export class AppModule {}
---
+++ backend/src/config/mysql.service.ts
@@ -0,0 +1,83 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as mysql from 'mysql2';
+import { Pool } from 'mysql2/promise';
+
+@Injectable()
+export class MySQLService implements OnModuleInit {
+  pool: Pool;
+  constructor(private configService: ConfigService) {
+    this.pool = mysql
+      .createPool({
+        host: configService.get('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        user: configService.get('DATABASE_USERNAME'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_DATABASE'),
+      })
+      .promise();
+  }
+
+  async onModuleInit() {
+    await this.pool.execute(`
+    CREATE TABLE IF NOT EXISTS REGION (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR(20) NOT NULL
+    )
+  `);
+
+    await this.pool.execute(`
+    CREATE TABLE IF NOT EXISTS USER (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      userId VARCHAR(20) NOT NULL,
+      password VARCHAR(20),
+      regionId int,
+      FOREIGN KEY (regionId) REFERENCES REGION(id)
+    )
+    `);
+
+    await this.pool.execute(`
+    CREATE TABLE IF NOT EXISTS CATEGORY (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR(20) NOT NULL
+    )
+    `);
+
+    await this.pool.execute(`
+    CREATE TABLE IF NOT EXISTS PRODUCT (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR(20) NOT NULL,
+      price DECIMAL(10, 0) NOT NULL,
+      createdAt TIMESTAMP NOT NULL,
+      isSold TINYINT(1) NOT NULL,
+      description VARCHAR(255) NOT NULL,
+      viewCount int,
+      thumbnails JSON,
+      categoryId int,
+      authorId int,
+      FOREIGN KEY (categoryId) REFERENCES CATEGORY(id),
+      FOREIGN KEY (authorId) REFERENCES USER(id)
+    )
+    `);
+
+    await this.pool.execute(`
+    CREATE TABLE IF NOT EXISTS USER_LIKE_PRODUCT (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      userId int NOT NULL,
+      productId int NOT NULL,
+      FOREIGN KEY (userId) REFERENCES USER(id),
+      FOREIGN KEY (productId) REFERENCES PRODUCT(id)
+    )
+    `);
+
+    await this.pool.execute(`
+    CREATE TABLE IF NOT EXISTS USER_VIEW_PRODUCT (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      userId int NOT NULL,
+      productId int NOT NULL,
+      FOREIGN KEY (userId) REFERENCES USER(id),
+      FOREIGN KEY (productId) REFERENCES PRODUCT(id)
+    )
+    `);
+  }
+}
