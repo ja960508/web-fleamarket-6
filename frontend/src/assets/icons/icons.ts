040e6beb9d1905a3199859141c188210034eb49c
jooam <lord0325@naver.com>
2022-08-18 17:45:53 +0900

    conf: Icon React Component로 export 설정

---
+++ frontend/src/assets/icons/icons.ts
@@ -0,0 +1,35 @@
+import { ReactComponent as AddIcon } from './addIcon.svg';
+import { ReactComponent as CategoryIcon } from './categoryIcon.svg';
+import { ReactComponent as CheckIcon } from './checkIcon.svg';
+import { ReactComponent as ChevronDownIcon } from './chevronDownIcon.svg';
+import { ReactComponent as ChevronLeftIcon } from './chevronLeftIcon.svg';
+import { ReactComponent as ChevronRightIcon } from './chevronRightIcon.svg';
+import { ReactComponent as CloseIcon } from './closeIcon.svg';
+import { ReactComponent as HeartIcon } from './heartIcon.svg';
+import { ReactComponent as UmageIcon } from './imageIcon.svg';
+import { ReactComponent as LogoutIcon } from './logoutIcon.svg';
+import { ReactComponent as MapPinIcon } from './mapPinIcon.svg';
+import { ReactComponent as MenuIcon } from './menuIcon.svg';
+import { ReactComponent as MessageSquareIcon } from './messageSquareIcon.svg';
+import { ReactComponent as MoreVerticalIcon } from './moreVerticalIcon.svg';
+import { ReactComponent as SendIcon } from './sendIcon.svg';
+import { ReactComponent as UserIcon } from './userIcon.svg';
+
+export {
+  AddIcon,
+  CategoryIcon,
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  CloseIcon,
+  HeartIcon,
+  UmageIcon,
+  LogoutIcon,
+  MapPinIcon,
+  MenuIcon,
+  MessageSquareIcon,
+  MoreVerticalIcon,
+  SendIcon,
+  UserIcon,
+};
