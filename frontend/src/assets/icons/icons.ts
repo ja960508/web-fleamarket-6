c44ca715c7d616f5a55669bc89a11396d50833e0
jooam <lord0325@naver.com>
2022-08-19 10:49:28 +0900

    fix: svg icon export 방식 변경

--- frontend/src/assets/icons/icons.ts
+++ frontend/src/assets/icons/icons.ts
@@ -1,35 +1,16 @@
-import { ReactComponent as AddIcon } from './addIcon.svg';
-import { ReactComponent as CategoryIcon } from './categoryIcon.svg';
-import { ReactComponent as CheckIcon } from './checkIcon.svg';
-import { ReactComponent as ChevronDownIcon } from './chevronDownIcon.svg';
-import { ReactComponent as ChevronLeftIcon } from './chevronLeftIcon.svg';
-import { ReactComponent as ChevronRightIcon } from './chevronRightIcon.svg';
-import { ReactComponent as CloseIcon } from './closeIcon.svg';
-import { ReactComponent as HeartIcon } from './heartIcon.svg';
-import { ReactComponent as UmageIcon } from './imageIcon.svg';
-import { ReactComponent as LogoutIcon } from './logoutIcon.svg';
-import { ReactComponent as MapPinIcon } from './mapPinIcon.svg';
-import { ReactComponent as MenuIcon } from './menuIcon.svg';
-import { ReactComponent as MessageSquareIcon } from './messageSquareIcon.svg';
-import { ReactComponent as MoreVerticalIcon } from './moreVerticalIcon.svg';
-import { ReactComponent as SendIcon } from './sendIcon.svg';
-import { ReactComponent as UserIcon } from './userIcon.svg';
-
-export {
-  AddIcon,
-  CategoryIcon,
-  CheckIcon,
-  ChevronDownIcon,
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  CloseIcon,
-  HeartIcon,
-  UmageIcon,
-  LogoutIcon,
-  MapPinIcon,
-  MenuIcon,
-  MessageSquareIcon,
-  MoreVerticalIcon,
-  SendIcon,
-  UserIcon,
-};
+export { ReactComponent as AddIcon } from './addIcon.svg';
+export { ReactComponent as CategoryIcon } from './categoryIcon.svg';
+export { ReactComponent as CheckIcon } from './checkIcon.svg';
+export { ReactComponent as ChevronDownIcon } from './chevronDownIcon.svg';
+export { ReactComponent as ChevronLeftIcon } from './chevronLeftIcon.svg';
+export { ReactComponent as ChevronRightIcon } from './chevronRightIcon.svg';
+export { ReactComponent as CloseIcon } from './closeIcon.svg';
+export { ReactComponent as HeartIcon } from './heartIcon.svg';
+export { ReactComponent as UmageIcon } from './imageIcon.svg';
+export { ReactComponent as LogoutIcon } from './logoutIcon.svg';
+export { ReactComponent as MapPinIcon } from './mapPinIcon.svg';
+export { ReactComponent as MenuIcon } from './menuIcon.svg';
+export { ReactComponent as MessageSquareIcon } from './messageSquareIcon.svg';
+export { ReactComponent as MoreVerticalIcon } from './moreVerticalIcon.svg';
+export { ReactComponent as SendIcon } from './sendIcon.svg';
+export { ReactComponent as UserIcon } from './userIcon.svg';
