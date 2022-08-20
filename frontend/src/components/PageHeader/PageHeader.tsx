a1244bf7ef16d629a31f9401f442539f70f816bf
Jinny <khj9709@icloud.com>
2022-08-20 19:34:58 +0900

    feat: PageHeader 컴포넌트 구현

--- frontend/src/components/PageHeader/PageHeader.tsx
+++ frontend/src/components/PageHeader/PageHeader.tsx
@@ -1,10 +1,43 @@
+import styled from 'styled-components';
+import { ChevronLeftIcon } from '../../assets/icons/icons';
+import { LinkButton } from '../../lib/Router';
+import colors from '../../styles/colors';
+import { textMedium } from '../../styles/fonts';
 interface PageHeaderProps {
   pageName: string;
   extraButton?: React.ReactNode;
 }
 
-function PageHeader() {
-  return <header>PageHeader</header>;
+function PageHeader({ pageName, extraButton }: PageHeaderProps) {
+  return (
+    <StyledHeader>
+      <LinkButton className="go-back-button" moveTo="/">
+        <ChevronLeftIcon />
+      </LinkButton>
+      <h1>{pageName}</h1>
+      {extraButton}
+    </StyledHeader>
+  );
 }
+
+const StyledHeader = styled.header`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  align-items: center;
+
+  padding: 1rem;
+  background-color: ${colors.offWhite};
+
+  & > .go-back-button {
+    display: flex;
+    justify-self: flex-start;
+  }
+
+  & > h1 {
+    justify-self: center;
+    ${textMedium};
+  }
+`;
 
 export default PageHeader;
