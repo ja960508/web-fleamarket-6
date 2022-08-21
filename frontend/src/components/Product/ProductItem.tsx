032e7e00a7f8ee77c1bd6a6574ec5d49aad66865
Jinny <khj9709@icloud.com>
2022-08-21 22:29:53 +0900

    feat: useQuery 적용해서 Product 데이터 불러오기

--- frontend/src/components/Product/ProductItem.tsx
+++ frontend/src/components/Product/ProductItem.tsx
@@ -1,19 +1,20 @@
 import { HeartIcon, MessageSquareIcon } from '../../assets/icons/icons';
 import { StyledProductItem } from './ProductItem.style';
 import { ProductPreviewType } from '../../types/product';
+import { parseDateFromNow } from '../../utils';
 
 function ProductItem({ product }: { product: ProductPreviewType }) {
   return (
     <StyledProductItem>
       <div>
-        <img src={product.thumbnail} alt="product_thumbnail" />
+        <img src="http://source.unsplash.com/random" alt="product_thumbnail" />
       </div>
       <div className="product-meta">
         <h4 className="product-name">{product.name}</h4>
         <div className="product-time-region">
           <span>{product.regionName}</span>
           <span className="delimiter"></span>
-          <span>{product.createdAt.length}</span>
+          <span>{parseDateFromNow(product.createdAt)}</span>
         </div>
         <strong className="product-price">{product.price}</strong>
         <div className="product-count-group">
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,26 +3,31 @@ import { useEffect, useState } from 'react';
 import HomeNavbar from '../components/HomeNavbar/HomeNavbar';
 import PostAddButton from '../components/Post/PostAddButton';
 import ProductItem from '../components/Product/ProductItem';
-import { useHistoryState } from '../lib/Router/hooks';
+import useQuery from '../hooks/useQuery';
+import { remote } from '../lib/api';
+import { useHistoryState, useSearchParams } from '../lib/Router/hooks';
 import { ProductPreviewType } from '../types/product';
 
 function Home() {
-  const [products, setProducts] = useState<ProductPreviewType[]>([]);
   const categoryIconURL = useHistoryState();
+  const searchParams = useSearchParams();
+  const categoryId = searchParams('categoryId');
 
-  useEffect(() => {
-    (async function () {
-      const { data } = await axios.get('/api/product');
-
-      setProducts(data);
-    })();
-  }, []);
+  const { data: products } = useQuery<ProductPreviewType[]>(
+    'products',
+    async () => {
+      const queryString = categoryId ? `?category=${categoryId}` : '';
+      const { data } = await remote.get('/product' + queryString);
+      return data.data;
+    },
+    [categoryId],
+  );
 
   return (
     <main>
       <HomeNavbar currentCategoryIcon={categoryIconURL} />
       <ul>
-        {products.map((product) => (
+        {products?.map((product) => (
           <ProductItem key={product.id} product={product} />
         ))}
       </ul>
