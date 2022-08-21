48d3fe18c66485c91ae4b28eb8f4d166a3df5b3b
Jinny <khj9709@icloud.com>
2022-08-21 16:47:27 +0900

    feat: query에 따라 paginated된 product 리스트 반환
    다음의 예외사항을 처리했어요.
    1. filter와 userId는 둘다 제공되어야함.
    2. filter와 category는 동시에 제공될 수 없음

--- backend/src/product/product.module.ts
+++ backend/src/product/product.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { S3Service } from 'src/config/s3/s3.service';
+import { MySQLModule } from 'src/config/mysql/mysql.module';
 
 @Module({
+  imports: [MySQLModule],
   controllers: [ProductController],
   providers: [ProductService, S3Service],
 })
--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -1,9 +1,72 @@
 import { HttpException, Injectable } from '@nestjs/common';
+import { Pool } from 'mysql2/promise';
+import { MySQLService } from 'src/config/mysql/mysql.service';
 import { S3Service } from 'src/config/s3/s3.service';
+import { ProductGetOptions } from './types/product';
 
 @Injectable()
 export class ProductService {
-  constructor(private readonly awsS3Service: S3Service) {}
+  pool: Pool;
+  constructor(
+    private readonly awsS3Service: S3Service,
+    private readonly mysqlService: MySQLService,
+  ) {
+    this.pool = mysqlService.pool;
+  }
+
+  async getProducts(options: ProductGetOptions) {
+    const LIMIT = 10;
+    const CALC_TOTAL_COUNT = `SQL_CALC_FOUND_ROWS`;
+    const { userId, filter, categoryId, page = 1 } = options;
+
+    if ((userId || filter) && categoryId) {
+      throw new HttpException(
+        'Filter cannot be provided with category option. Choose one.',
+        400,
+      );
+    }
+
+    if (Boolean(userId) !== Boolean(filter)) {
+      throw new HttpException(
+        'userId and filter both should be provided.',
+        400,
+      );
+    }
+
+    let beforePaginationQuery = /*sql*/ `SELECT ${CALC_TOTAL_COUNT} * FROM PRODUCT`;
+
+    if (filter === 'like') {
+      beforePaginationQuery = /*sql*/ `
+          select ${CALC_TOTAL_COUNT} P.* from PRODUCT as P inner join USER_LIKE_PRODUCT as ULP on ULP.productId = P.id where ULP.userId = ${userId}
+        `;
+    }
+
+    if (filter === 'sale') {
+      beforePaginationQuery = /*sql*/ `
+          select ${CALC_TOTAL_COUNT} PRODUCT.* from PRODUCT where authorId = ${userId}
+        `;
+    }
+
+    if (categoryId) {
+      beforePaginationQuery = /*sql*/ `
+        select ${CALC_TOTAL_COUNT} PRODUCT.* from PRODUCT where categoryId = ${categoryId}
+      `;
+    }
+
+    const paginatedQuery = /*sql*/ `${beforePaginationQuery} LIMIT ${
+      (page - 1) * LIMIT
+    }, ${LIMIT}`;
+
+    const [result] = await this.pool.query(paginatedQuery);
+    const [[{ totalCount }]] = (await this.pool.query(
+      /*sql*/ `SELECT FOUND_ROWS() as totalCount`,
+    )) as any[][];
+
+    return {
+      totalCount,
+      data: result,
+    };
+  }
 
   async uploadThumbnails(thumbnails: Array<Express.Multer.File>) {
     try {
