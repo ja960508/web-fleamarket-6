e96e6482b36815805a3b6d2240e689a5677ed1cf
jooam <lord0325@naver.com>
2022-08-22 21:54:49 +0900

    feat: 페이지 헤더 추가

--- frontend/src/pages/Auth/SignIn.tsx
+++ frontend/src/pages/Auth/SignIn.tsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from 'react';
 import styled from 'styled-components';
 import CustomInput from '../../components/CustomInput';
+import PageHeader from '../../components/PageHeader/PageHeader';
 import { UserInfoDispatch } from '../../context/UserInfoContext';
 import { remote } from '../../lib/api';
 import { Link, useNavigate } from '../../lib/Router';
@@ -37,31 +38,34 @@ function SignIn() {
   };
 
   return (
-    <StyledSignInForm onSubmit={handleLogin}>
-      <CustomInput
-        type="text"
-        name="nickname"
-        placeholder="닉네임을 입력하세요"
-        value={nickname}
-        onChange={(event) => setNickname(event.target.value)}
-      />
-      <CustomInput
-        type="password"
-        name="password"
-        placeholder="비밀번호를 입력하세요"
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-      />
-      <button type="submit">로그인</button>
-      <StyledGithubOAuthLink
-        href={`https://github.com/login/oauth/authorize?client_id=${
-          import.meta.env.VITE_CLIENT_ID
-        }`}
-      >
-        깃허브로 로그인
-      </StyledGithubOAuthLink>
-      <Link to="/auth/sign-up">회원가입</Link>
-    </StyledSignInForm>
+    <>
+      <PageHeader pageName="로그인" />
+      <StyledSignInForm onSubmit={handleLogin}>
+        <CustomInput
+          type="text"
+          name="nickname"
+          placeholder="닉네임을 입력하세요"
+          value={nickname}
+          onChange={(event) => setNickname(event.target.value)}
+        />
+        <CustomInput
+          type="password"
+          name="password"
+          placeholder="비밀번호를 입력하세요"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit">로그인</button>
+        <StyledGithubOAuthLink
+          href={`https://github.com/login/oauth/authorize?client_id=${
+            import.meta.env.VITE_CLIENT_ID
+          }`}
+        >
+          깃허브로 로그인
+        </StyledGithubOAuthLink>
+        <Link to="/auth/sign-up">회원가입</Link>
+      </StyledSignInForm>
+    </>
   );
 }
 
