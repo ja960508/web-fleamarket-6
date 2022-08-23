8bd7479efebad36c4513779748ace2c661e7547d
Jinny <khj9709@icloud.com>
2022-08-23 23:55:09 +0900

    feat: 드랍다운 컴포넌트 구현

---
+++ frontend/src/components/commons/Dropdown.tsx
@@ -0,0 +1,85 @@
+import { MouseEvent, useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+import colors from '../../styles/colors';
+import { textSmall } from '../../styles/fonts';
+import mixin from '../../styles/mixin';
+
+interface DropDownElementInfo {
+  text: string;
+  onClick: () => void;
+}
+
+interface DropDownProps {
+  initialDisplay: JSX.Element;
+  dropDownElements: DropDownElementInfo[];
+}
+
+function DropDown({ initialDisplay, dropDownElements }: DropDownProps) {
+  const dropDownRef = useRef<HTMLButtonElement>(null);
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+
+  const toggleDropDown = (force?: boolean) => {
+    setIsDropDownOpen((prevIsOpen) =>
+      force === undefined ? !prevIsOpen : force,
+    );
+  };
+
+  useEffect(() => {
+    const handleClickOutsideDropdown = ({ target }: Event) => {
+      if (!(target instanceof HTMLElement) || !dropDownRef.current) return;
+
+      if (dropDownRef.current.contains(target)) return;
+
+      toggleDropDown(false);
+    };
+
+    document.addEventListener('click', handleClickOutsideDropdown);
+    return () => {
+      document.removeEventListener('click', handleClickOutsideDropdown);
+    };
+  }, []);
+
+  return (
+    <StyledDropDown ref={dropDownRef} onClick={() => toggleDropDown()}>
+      {initialDisplay}
+      {isDropDownOpen && (
+        <DropDownList>
+          {dropDownElements.map(({ text, onClick }) => (
+            <DropDownElement key={text} onClick={onClick}>
+              {text}
+            </DropDownElement>
+          ))}
+        </DropDownList>
+      )}
+    </StyledDropDown>
+  );
+}
+
+const StyledDropDown = styled.button`
+  position: relative;
+`;
+
+const DropDownList = styled.ul`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  transform: translateY(100%);
+
+  width: 8rem;
+
+  display: flex;
+  flex-direction: column;
+
+  ${mixin.shadow.normal};
+  backdrop-filter: blur(4px);
+  background-color: ${colors.offWhite};
+  border-radius: 10px;
+`;
+
+const DropDownElement = styled.li`
+  ${textSmall};
+  padding: 1rem;
+  text-align: left;
+`;
+
+export default DropDown;
