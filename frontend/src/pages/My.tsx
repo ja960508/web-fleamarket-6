8777ca9b177dc1d93e7688c1acaeac0722e87594
jooam <lord0325@naver.com>
2022-08-24 17:53:17 +0900

    feat: 마이페이지 상단 마크업 및 스타일링

--- frontend/src/pages/My.tsx
+++ frontend/src/pages/My.tsx
@@ -1,5 +1,60 @@
+import { useContext, useState } from 'react';
+import styled from 'styled-components';
+import PageHeader from '../components/PageHeader/PageHeader';
+import { UserInfoDispatch } from '../context/UserInfoContext';
+import { credentialRemote } from '../lib/api';
+import { useNavigate } from '../lib/Router';
+import colors from '../styles/colors';
+
+const TABS = ['판매목록', '채팅', '관심목록'];
+
 function My() {
-  return <div>My</div>;
+  const [tab, selectedTab] = useState(TABS[0]);
+  const dispatch = useContext(UserInfoDispatch);
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await credentialRemote.get('auth/logout');
+    dispatch({ type: 'USERINFO/DELETE_USER' });
+
+    navigate('/');
+  };
+
+  return (
+    <>
+      <PageHeader
+        pageName="마이페이지"
+        extraButton={<button onClick={handleLogout}>로그아웃</button>}
+      />
+      <StyledMyPageTabs>
+        {TABS.map((item) => (
+          <li
+            key={item}
+            className={tab === item ? 'selected' : ''}
+            onClick={() => selectedTab(item)}
+          >
+            {item}
+          </li>
+        ))}
+      </StyledMyPageTabs>
+    </>
+  );
 }
 
 export default My;
+
+const StyledMyPageTabs = styled.ul`
+  display: flex;
+  justify-content: space-around;
+  padding-top: 1rem;
+  background-color: ${colors.offWhite};
+
+  li {
+    padding: 0 0.5rem 1rem 0.5rem;
+  }
+
+  .selected {
+    color: ${colors.primary};
+    border-bottom: 2px solid ${colors.primary};
+  }
+`;
