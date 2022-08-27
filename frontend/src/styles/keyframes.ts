130a2ac6cfcb949ac7acfa11276478ca9e1d158d
Jinny <khj9709@icloud.com>
2022-08-27 20:17:23 +0900

    style: notfound 페이지 스타일링

---
+++ frontend/src/styles/keyframes.ts
@@ -0,0 +1,19 @@
+import { keyframes } from 'styled-components';
+
+export const BannerAnimation = keyframes`
+  0% {
+    opacity: 0.5;
+  }
+
+  50% {
+    opacity: 1;
+  }
+
+  75% {
+    opcity: 0.75;
+  }
+
+  100% {
+    opacity: 0.5;
+  }
+`;
